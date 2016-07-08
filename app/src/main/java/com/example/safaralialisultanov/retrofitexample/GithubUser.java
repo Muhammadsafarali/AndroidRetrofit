ff7c59129d9811fdabfb2eeaa914707b7ce2dffa
Алисултанов Сафарали Энверович <safarali.alisultanov@norbit.ru>
2016-07-08 16:44:14 +0300

    GithubAPI

---
+++ app/src/main/java/com/example/safaralialisultanov/retrofitexample/GithubUser.java
@@ -0,0 +1,15 @@
+package com.example.safaralialisultanov.retrofitexample;
+
+/**
+ * Created by safarali.alisultanov on 08.07.2016.
+ */
+public class GithubUser {
+
+    String login;
+    String name;
+
+    @Override
+    public String toString() {
+        return(login);
+    }
+}
---
+++ app/src/main/java/com/example/safaralialisultanov/retrofitexample/GithubUserAPI.java
@@ -0,0 +1,16 @@
+package com.example.safaralialisultanov.retrofitexample;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by safarali.alisultanov on 08.07.2016.
+ */
+public interface GithubUserAPI {
+
+    String ENPOINT = "https://api.github.com";
+
+    @GET("/users/{user}")
+    Call<GithubUser> getUser(@Path("user") String user);
+}
